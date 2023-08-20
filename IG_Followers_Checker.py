0ba84d360869da09953818254d8d1cf3660298a2
Veer Mehta <57525741+codesbyveer09@users.noreply.github.com>
2023-08-20 10:24:47 +0530

    Update IG_Followers_Checker.py

--- IG_Followers_Checker.py
+++ IG_Followers_Checker.py
@@ -1,4 +1,4 @@
-import time, random, csv
+import time
 from selenium import webdriver
 from selenium.webdriver.edge import service
 from selenium.webdriver.common.keys import Keys
@@ -7,13 +7,13 @@ from selenium.webdriver import ActionChains
 
 
 """
-install python:				https://www.python.org/downloads/
+install python3:				https://www.python.org/downloads/
 --> Custom installation --> Add to Path
 
 install selenium:
 pip install selenium
 
-download chrome webdriver and keep it in the same folder as the program
+download chrome webdriver and save it in the same folder as the program
 """
 
 
@@ -22,43 +22,42 @@ s=service.Service('chromedriver.exe')
 
 l_abt = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 log = []
-unm = "veer_0909"
-pwd = "VAM#090905"
+UNM = "YOUR_USERNAME"
+PWD = "YOUR_PASSWORD"
+RECORDS = 100							#Maximum amount among Followers and Following
+SCROLL_AMT = RECORDS//12
 browser = webdriver.Chrome(service = s, options = chrome_options)
 browser.maximize_window()
 browser.get("https://www.instagram.com")
 time.sleep(2)
-uname = browser.find_element("xpath", "//input[@type = 'text']")
-uname.send_keys(unm)
-paswd = browser.find_element("xpath", "//input[@type = 'password']")
-paswd.send_keys(pwd)
-submit = browser.find_element("xpath", "//button[@type = 'submit']")
-submit.click()
+browser.find_element("xpath", "//input[@type = 'text']").send_keys(UNM)
+browser.find_element("xpath", "//input[@type = 'password']").paswd.send_keys(PWD)
+browser.find_element("xpath", "//button[@type = 'submit']").click()
 time.sleep(5)
 
 l_following, l_followers = [],[]
 
-browser.get("https://www.instagram.com/"+unm+"/followers")
+browser.get("https://www.instagram.com/"+UNM+"/followers")
 time.sleep(2)
 body = browser.find_element("css selector",'body')
 
 ac = ActionChains(browser)
 ac.move_to_element(body).move_by_offset(0,0).click().perform()
-for i in range(40):
+for i in range(SCROLL_AMT):
 	body.send_keys(Keys.PAGE_DOWN)
 	time.sleep(1)
 
-a = browser.find_elements("xpath", "//div[@class='x9f619 xjbqb8w x1rg5ohu x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x6s0dn4 x1oa3qoh x1nhvcw1']")
+a = browser.find_elements("xpath", "//div[@class='x9f619 xjbqb8w x1rg5ohu x168nmei x103lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x6s0dn4 x1oa3qoh x1nhvcw1']")
 for i in a:
 	l_followers.append(i.text)
 
 time.sleep(2)
-browser.get("https://www.instagram.com/"+unm+"/following")
+browser.get("https://www.instagram.com/"+UNM+"/following")
 time.sleep(5)
 body = browser.find_element("css selector",'body')
 ac = ActionChains(browser)
 ac.move_to_element(body).move_by_offset(0,0).click().perform()
-for i in range(40):
+for i in range(SCROLL_AMT):
 	body.send_keys(Keys.PAGE_DOWN)
 	time.sleep(1)
 a = browser.find_elements("xpath", "//div[@class='x9f619 xjbqb8w x1rg5ohu x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x6s0dn4 x1oa3qoh x1nhvcw1']")
